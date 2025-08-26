9dd7bf9f9054d14965f03ec06c7b63b7366a5f69
ramya064 <ramyamaddala621@gmail.com>
2025-08-26 21:19:39 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./layout/Navbar";
+import Home from "./pages/home";
+import { BrowserRouter as Router, Route,  Routes } from 'react-router-dom';
+import AddUsers from './users/AddUsers';
+import EditUsers from './users/EditUsers';
+import ViewUsers from './users/ViewUsers';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="App">
+    <Router>
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<Home />}/>
+      <Route exact path="/adduser" element={<AddUsers/>}/>
+      <Route exact path="/edituser/:id" element={<EditUsers/>}/>
+              <Route path="/viewuser/:id" element={<ViewUsers />} />
+
+    </Routes>
+    </Router>
+      
     </div>
   );
 }
